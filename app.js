e32460c89de1a94f88d3bcbf4d5182b7a568e7eb
alifaisal987 <alifaisal987@gmail.com>
2024-07-07 15:31:48 +0500

    if else

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-var age= +prompt('Enter Your Age')
+// var age= +prompt('Enter Your Age')
 
-if (age>=18) {
-    alert("you are old enough access granted ")
+// if (age>=18) {
+//     alert("you are old enough access granted ")
 
+// }
+// else{
+//     alert('you are under 18 access denied')
+// }
+
+
+
+var userchar = prompt('Enter the character')
+
+if (userchar.length>1 ){
+//   prompt('Enter the characer')
+  console.log(userchar.length)
 }
-else{
-    alert('you are under 18 access denied')
+else if (userchar=a){
+    console.log('it is a vowel')
 }
+
